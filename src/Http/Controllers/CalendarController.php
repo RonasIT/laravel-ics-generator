6e8cbcbb731ec1df9ed5683c6f8e3d0bd6953dbf
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-04-10 09:58:10 +0200

    Ad try catch for IcalendarException

--- src/Http/Controllers/CalendarController.php
+++ src/Http/Controllers/CalendarController.php
@@ -17,15 +17,18 @@ use Symfony\Component\HttpFoundation\Response;
 class CalendarController extends Controller
 {
     public function export(ExportCalendarRequest $request) {
-        $ical = (new Ical())
-            ->setAddress($request->input('address'))
-            ->setDateStart(new DateTime($request->input('from')))
-            ->setDateEnd(new DateTime($request->input('to')))
-            ->setDescription($request->input('description'))
-            ->setSummary($request->input('name'))
-            ->setOrganizer($request->input('contact_email'))
-            ->setFilename(uniqid());
-
+        try {
+            $ical = (new Ical())
+                ->setAddress($request->input('address'))
+                ->setDateStart(new DateTime($request->input('from')))
+                ->setDateEnd(new DateTime($request->input('to')))
+                ->setDescription($request->input('description'))
+                ->setSummary($request->input('name'))
+                ->setOrganizer($request->input('contact_email'))
+                ->setFilename(uniqid());
+        } catch (IcalendarException $exc) {
+            //Something
+        }
         return response($ical->getICAL(), Response::HTTP_OK, [
             'Content-type' => 'text/calendar; charset=utf-8',
             'Content-Disposition' => 'attachment; filename=' . $ical->getFilename() . '.ics'
